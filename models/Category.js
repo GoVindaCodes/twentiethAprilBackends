7cfeea9bec865b95d0405263dc492ff95278984d
GoVindaCodes <govindayadav2478@gmail.com>
2024-04-19 18:16:03 +0530

    first commit

---
+++ models/Category.js
@@ -0,0 +1,95 @@
+// const mongoose = require('mongoose');
+// const { Children } = require('react');
+
+// const categorySchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: Array,
+//       required: true,
+//     },
+//     description: {
+//       type: Object,
+//       required: false,
+//     },
+//     slug: {
+//       type: String,
+//       required: false,
+//     },
+//     parentId: {
+//       type: String,
+//       required: false,
+//     },
+//     parentName: {
+//       type: String,
+//       required: false,
+//     },
+//     id: {
+//       type: String,
+//       required: false,
+//     },
+//     icon: {
+//       type: String,
+//       required: false,
+//     },
+//     status: {
+//       type: String,
+//       lowercase: true,
+//       enum: ['show', 'hide'],
+//       default: 'show',
+//     },
+//     // Children: {
+//     //   type: String,
+//     //   required: true,
+//     // }
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = categorySchema;
+
+// const Category = mongoose.model('Category', categorySchema);
+// module.exports = Category;
+
+
+
+// just testing according to utilss of frontend
+
+
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+
+  // just for testing its commented out uncomment when needed
+  _id: {
+    type: String,
+    // required: true,
+    required: false,
+  },
+  children: {
+    type: [String],
+    required: false, // Change to false if children are not always present
+  },
+  parent: {
+    type: String,
+    required: true,
+    required: false, // Change this to 'false' if the parent field should be optional
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Show",
+    enum: ["Show", "Hide"], // Assuming status can be "Show" or "Hide"
+  },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+module.exports = Category;
